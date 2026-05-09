2720b301e3237127d01bf29c531fd7d6487381f7
Chavi Budhwar <151002923+ChaviBudhwar@users.noreply.github.com>
2026-05-09 10:57:19 +0000

    text wrap, 09/05(need to fix call card text)

--- bingo.py
+++ bingo.py
@@ -3,6 +3,7 @@ import random          # To shuffle the photos
 import tkinter as tk   # To make the window 
 from tkinter import filedialog, messagebox # For pop-ups
 from PIL import Image, ImageDraw, ImageFont # For the photos and text
+import textwrap
 
 def browse_folder():
     folder_selected = filedialog.askdirectory()
@@ -86,14 +87,18 @@ def generate_bingo():
                     card_canvas.paste(img, (center_x, center_y))
             
                 label = os.path.splitext(os.path.basename(img_path))[0]
-            
+                dynamic_size = max(20, cell_size // 10)
                 try:
-                    font = ImageFont.load_default(size=60)
+                    #font = ImageFont.load_default(size=60)
+                    font = ImageFont.truetype("arial.ttf", dynamic_size)
                 except:
-                    font = ImageFont.load_default() 
+                    font = ImageFont.load_default(size=60) 
+                wrapper = textwrap.TextWrapper(width=15)
+                wrapped_label = wrapper.fill(text=label)
+
 
                 text_y = y + int(cell_size * 0.82)
-                draw.text((x + cell_size//2, text_y), label, fill="black", font=font, anchor="ms")
+                draw.multiline_text((x + cell_size//2, text_y), wrapped_label, fill="black", font=font, anchor="ms")
 
                 draw.rectangle([x, y, x + cell_size, y + cell_size], outline="black", width=3)
 
@@ -104,12 +109,10 @@ def generate_bingo():
         print(f"Saved: {card_filename}")
 
 
-    messagebox.showinfo("Status", "🎉 All Bingo cards have been generated successfully!")
-    messagebox.showinfo("Status", f"Check the '{output_folder}' folder to see your cards.")
+    
 
 
-
-    messagebox.showinfo("Status", "Creating the Master Call Card...")
+    print("Status", "Creating the Master Call Card...")
 
     final_call_list = sorted(list(used_images_master))
 
@@ -125,7 +128,17 @@ def generate_bingo():
     master_h = rows * master_cell_size
     master_card = Image.new("RGB", (master_w, master_h), "white")
     master_draw = ImageDraw.Draw(master_card)
+    try:
+        master_font = ImageFont.truetype("arial.ttf", 60)
+    except:
+        master_font = ImageFont.load_default(size=40)
+    wrapper = textwrap.TextWrapper(width=15)
+    wrapped_label = wrapper.fill(text=label)
 
+
+    text_y = y + int(cell_size * 0.82)
+    draw.multiline_text((x + cell_size//2, text_y), wrapped_label, fill="black", font=font, anchor="ms")
+    
 
     for index, img_path in enumerate(final_call_list):
 
@@ -156,7 +169,10 @@ def generate_bingo():
 
 
     master_card.save(f"{output_folder}/_MASTER_CALL_CARD.png")
-    messagebox.showinfo("Status", "🎉 Master Call Card saved as '_MASTER_CALL_CARD.png'")
+    print("Status", "🎉 Master Call Card saved as '_MASTER_CALL_CARD.png'")
+
+    messagebox.showinfo("Status", "🎉 All Bingo cards have been generated successfully!")
+    messagebox.showinfo("Status", f"Check the '{output_folder}' folder to see your cards.")
 
 
 root = tk.Tk()
